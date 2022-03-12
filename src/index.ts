503b468bb4e89b31cf9ebd538d5916144d349907
Serhii Merkulov <smerkulov@c.snap.com>
2022-03-12 22:26:47 +0200

    added base scene and controls

--- src/index.ts
+++ src/index.ts
@@ -1,63 +1,90 @@
 import * as PIXI from "pixi.js"
+import { Pool } from "./pool"
 
-function main() {
-    const app = new PIXI.Application({ backgroundColor: 0x1099bb });
-    document.body.appendChild(app.view);
+const app: PIXI.Application = new PIXI.Application({ width: 320, height: 640, backgroundColor: 0x1099bb })
+app.ticker.add(update)
 
-    const basicText = new PIXI.Text('Basic text in pixi');
-    basicText.x = 50;
-    basicText.y = 100;
+document.body.appendChild(app.view)
 
-    app.stage.addChild(basicText);
+const cubeTexture: PIXI.Texture = PIXI.Texture.from("./assets/cube.jpg")
+const CUBE_SIZE: number = 32
 
-    const style = new PIXI.TextStyle({
-        fontFamily: 'Arial',
-        fontSize: 36,
-        fontStyle: 'italic',
-        fontWeight: 'bold',
-        fill: ['#ffffff', '#00ff99'], // gradient
-        stroke: '#4a1850',
-        strokeThickness: 5,
-        dropShadow: true,
-        dropShadowColor: '#000000',
-        dropShadowBlur: 4,
-        dropShadowAngle: Math.PI / 6,
-        dropShadowDistance: 6,
-        wordWrap: true,
-        wordWrapWidth: 440,
-        lineJoin: 'round',
-    });
+const buttonLeft: PIXI.Sprite = PIXI.Sprite.from(cubeTexture)
+buttonLeft.alpha = 0
+buttonLeft.anchor.x = 0
+buttonLeft.anchor.y = 0
+buttonLeft.position.x = 0
+buttonLeft.position.y = 0
+buttonLeft.scale.x = app.screen.width / CUBE_SIZE / 2
+buttonLeft.scale.y = app.screen.height / CUBE_SIZE
+buttonLeft.buttonMode = true
+buttonLeft.interactive = true
+buttonLeft.on("pointerdown", onLeft)
+buttonLeft.on("pointerup", onRelease)
+app.stage.addChild(buttonLeft)
 
-    const richText = new PIXI.Text('Rich text with a lot of options and across multiple lines', style);
-    richText.x = 50;
-    richText.y = 220;
+const buttonRight: PIXI.Sprite = PIXI.Sprite.from(cubeTexture)
+buttonRight.alpha = 0
+buttonRight.anchor.x = 1
+buttonRight.anchor.y = 0
+buttonRight.position.x = app.screen.right
+buttonRight.position.y = 0
+buttonRight.scale.x = app.screen.width / CUBE_SIZE / 2
+buttonRight.scale.y = app.screen.height / CUBE_SIZE
+buttonRight.buttonMode = true
+buttonRight.interactive = true
+buttonRight.on("pointerdown", onRight)
+buttonRight.on("pointerup", onRelease)
+app.stage.addChild(buttonRight)
 
-    app.stage.addChild(richText);
+const wallLeft: PIXI.Sprite = PIXI.Sprite.from(cubeTexture)
+wallLeft.anchor.x = 0
+wallLeft.anchor.y = 1
+wallLeft.scale.x = 0.5
+wallLeft.scale.y = 9
+wallLeft.position.x = 0
+wallLeft.position.y = app.screen.bottom
+app.stage.addChild(wallLeft)
 
-    const skewStyle = new PIXI.TextStyle({
-        fontFamily: 'Arial',
-        dropShadow: true,
-        dropShadowAlpha: 0.8,
-        dropShadowAngle: 2.1,
-        dropShadowBlur: 4,
-        dropShadowColor: '0x111111',
-        dropShadowDistance: 10,
-        fill: ['#ffffff'],
-        stroke: '#004620',
-        fontSize: 60,
-        fontWeight: 'lighter',
-        lineJoin: 'round',
-        strokeThickness: 12,
-    });
+const wallCenter: PIXI.Sprite = PIXI.Sprite.from(cubeTexture)
+wallCenter.anchor.x = 0.5
+wallCenter.anchor.y = 1
+wallCenter.scale.x = 0.5
+wallCenter.scale.y = 6
+wallCenter.position.x = app.screen.width / 2
+wallCenter.position.y = app.screen.bottom
+app.stage.addChild(wallCenter)
 
-    const skewText = new PIXI.Text('SKEW IS COOL', skewStyle);
-    skewText.skew.set(0.65, -0.3);
-    skewText.anchor.set(0.5, 0.5);
-    skewText.x = 300;
-    skewText.y = 480;
+const wallRight: PIXI.Sprite = PIXI.Sprite.from(cubeTexture)
+wallRight.anchor.x = 1
+wallRight.anchor.y = 1
+wallRight.scale.x = 0.5
+wallRight.scale.y = 9
+wallRight.position.x = app.screen.right
+wallRight.position.y = app.screen.bottom
+app.stage.addChild(wallRight)
 
-    app.stage.addChild(skewText);
+const cap: PIXI.Sprite = PIXI.Sprite.from(cubeTexture)
+cap.anchor.x = 0.5
+cap.anchor.y = 1
+cap.scale.x = 0.5
+cap.scale.y = 6
+cap.position.x = app.screen.width / 2
+cap.position.y = app.screen.bottom - 32 * 6
+app.stage.addChild(cap)
 
+function update(): void {
+    
 }
 
-main()
+function onLeft(): void {
+    cap.rotation = -Math.PI * 1 / 3
+}
+
+function onRight(): void {
+    cap.rotation = Math.PI * 1 / 3
+}
+
+function onRelease(): void {
+    cap.rotation = 0
+}
